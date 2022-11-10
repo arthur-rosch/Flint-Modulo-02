16cc1852749470abb0b334fc411a59ac4c2ce5c2
Arthur Rosch <rosch.arthur@gmail.com>
2022-11-09 21:21:11 -0300

    Exec 1.1 e 1.2 finalizado

---
+++ Exec 1.1,1.2/script.js
@@ -0,0 +1,10 @@
+let soma = 1 + 1,
+  subtracao = 2 - 2,
+  divisao = 80 / 3,
+  resto = 80 % 3;
+soma = +5;
+soma = -3;
+
+//Se_caminho_5km && corro_2km, passo_o_dia_bem
+//Se_levanto_cedo || antes_das_8h, saio_tranquilo
+//Se_NAO_termino_o_trabalho || Se_NAO_tenho_compromisso,_saio_tarde
